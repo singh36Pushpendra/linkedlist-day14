6b480ae43f01bf434fa7e71318b17c9d52fb8ccf
singh36Pushpendra <psthakur.1999@gmail.com>
2022-10-09 19:07:39 +0530

    Added: First commit

---
+++ src/ds/linear/dynamic/LinkedList.java
@@ -0,0 +1,39 @@
+package ds.linear.dynamic;
+
+public class LinkedList<T> {
+
+	Node head;
+	
+	class Node {
+		T item;
+		Node ref;
+		
+		Node(T item) {
+			this.item = item;
+		}
+		
+	}
+	
+	public void add(T item) {
+		Node newNode = new Node(item);
+		if (head == null)
+			head = newNode;
+		else {
+			Node temp = head;
+			while (temp.ref != null)
+				temp = temp.ref;
+			temp.ref = newNode;
+		}
+	}
+	
+	public void display() {
+		Node temp = head;
+		while (temp != null) {
+			if (temp.ref != null)
+				System.out.print(temp.item + " -> ");
+			else
+				System.out.println(temp.item);
+			temp = temp.ref;
+		}
+	}
+}
---
+++ src/ds/linear/dynamic/LinkedListDemo.java
@@ -0,0 +1,13 @@
+package ds.linear.dynamic;
+
+public class LinkedListDemo {
+
+	public static void main(String[] args) {
+		LinkedList<Integer> integers = new LinkedList<Integer>();
+		integers.add(56);
+		integers.add(30);
+		integers.add(70);
+		integers.display();
+	}
+
+}
